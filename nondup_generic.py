41353edcaef63cda30f8c9afc26a3172ad7bbbfa
Janardhan Shetty <jshetty@jshetty-ltm7.internal.salesforce.com>
2015-09-16 20:05:42 -0700

    Added code for last element

--- nondup_generic.py
+++ nondup_generic.py
@@ -4,20 +4,23 @@ from __future__ import print_function
 
 def main():
     ''' Print all non-dup items in an array, all other items can repeat many times '''
-    arr = [2, 2, 2, 2, 3, 4, 4, 5, 7, 7, 7, 8, 9, 9, 9, 9, 10, 11]
+    arr = [2, 2, 2, 10, 11, 12, 12]
     repeat = 0;
     for i in range(len(arr)):
-        if i == len(arr) - 1 and repeat == 0 :
+        if i == len(arr) - 1 and not repeat:
             print("Unique element = ", arr[i])
             break
-        if arr[i] == arr[i+1]:
-            repeat = 1
-        else:
-            if not repeat:
-                print("Unique element = ", arr[i])
+        if i == len(arr) - 1 and repeat:
+           pass
+        if i < len(arr) - 1: 
+            if arr[i] == arr[i+1]:
+                repeat = 1
             else:
-                repeat = 0
-            
+                if not repeat:
+                    print("Unique element = ", arr[i])
+                else:
+                    repeat = 0
+
  
 if __name__ == "__main__":
     main()
